fee73390a91ceb7d8ae279310eb24ca7703ba54d
Nikkie Mashian <nikkiemashian@gmail.com>
2019-01-08 14:07:02 -0800

    user onboarding

--- src/components/brief-about.js
+++ src/components/brief-about.js
@@ -12,9 +12,17 @@ export class AboutPage extends React.Component{
           <h1>Welcome To Pawfiles</h1>
           <p>
             Wouldn't it be nice to keep a timeline of your pet's life? From their medical records to remembering something hilarious they did the other day, it can be easy to forget important things about your furry best friend.
-            <br/><br/>
-            Well Pawfiles is here to help you keep track of your pet's most important moments! Whether it's what the vet said at their last appointment, a record of their vaccination schedule, or a cute memory you just don't want to forget, this is the place to document it all!
+            Well Pawfiles is here to help you keep track of your pet's most important moments! Whether it's what the vet said at their last appointment or a cute memory you just don't want to forget, this is the place to document it all!
           </p>
+          <h3>What You Can Do:</h3>
+          <ul>
+            <li>Create medical posts to document symptoms, vet diagnosis, medications, and vaccinations</li>
+            <li>Create memory posts with a cute picture to remember the moment</li>
+            <li>Create reminders for each pet, with an optional time or date</li>
+            <li>Search for a memory or medical post conveniently</li>
+            <li>Easily keep track of each pet's doctors, vaccination schedule, and medical history</li>
+            <li>Have as many personalized Pawfiles as you do pets!</li>
+          </ul>
         </div>
         
       </div>
