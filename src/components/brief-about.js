e0188487780ab661aa21ffc2d913b23c70a5d885
Nikkie Mashian <nikkiemashian@gmail.com>
2019-01-08 13:46:11 -0800

    adding a list on about blurb

--- src/components/brief-about.js
+++ src/components/brief-about.js
@@ -9,10 +9,12 @@ export class AboutPage extends React.Component{
     return(
       <div className="brief-about">
         <div className="center-me">
-        <h1>Welcome To Pawfiles</h1>
-        <p>
-          Wouldn't it be nice to keep a timeline of your pet's life? From their medical records to remembering something hilarious they did the other day, it can be easy to forget important things about your furry best friend. Well now you can keep track of it all with Pawfiles! Pawfiles helps you keep a simple timeline on your pet: whether it's what the vet said at their last appointment, a record of their vaccination schedule, or a cute memory you just don't want to forget, this is the place to document it all!
-        </p>
+          <h1>Welcome To Pawfiles</h1>
+          <p>
+            Wouldn't it be nice to keep a timeline of your pet's life? From their medical records to remembering something hilarious they did the other day, it can be easy to forget important things about your furry best friend.
+            <br/><br/>
+            Well Pawfiles is here to help you keep track of your pet's most important moments! Whether it's what the vet said at their last appointment, a record of their vaccination schedule, or a cute memory you just don't want to forget, this is the place to document it all!
+          </p>
         </div>
         
       </div>
