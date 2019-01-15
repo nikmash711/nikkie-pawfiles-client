920ab0848ce8009d43ae02a82a421503ed22b710
Nikkie Mashian <nikkiemashian@gmail.com>
2019-01-14 18:53:45 -0800

    removed addpawfilebutton and sortbyselect components, added them directly into mypawfiles.js since they were unnecassary as their own components

--- src/components/home/my-pawfiles.js
+++ src/components/home/my-pawfiles.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import PawfilesList from './pawfiles-list';
-import AddNewPawfileButton from './add-new-pawfile-button';
 import PawfileForm from '../pawfile-form';
-import SortBySelect from './sort-by-select';
 import Header from './header';
 import LoadingAnimation from '../loading-animation'
-import {changeSearchTerm} from '../../actions/index';
+import {changeSearchTerm, showPawfileForm, changeSortingPetsMethod} from '../../actions/index';
 import './my-pawfiles.css'
 
 export class MyPawfiles extends React.Component{
@@ -39,9 +37,19 @@ export class MyPawfiles extends React.Component{
         {error}
           <Header/>
           <nav className="options">
-            {this.props.pawfiles.length>1 && <SortBySelect/>}
+            {
+              this.props.pawfiles.length>1 
+              && 
+              <select className="tape sort" onChange={e=>this.props.dispatch(changeSortingPetsMethod(e.target.value))}>
+                <option value="">Sort By:</option>
+                <option value="oldest">Oldest</option>
+                <option value="youngest">Youngest</option>
+                <option value="A-Z">A-Z</option>
+                <option value="Z-A">Z-A</option>
+              </select>
+            }
             {this.props.pawfiles.length>1 && <input onChange={e=>this.props.dispatch(changeSearchTerm(e.target.value))} className="tape" placeholder='Search Pets'/>}
-            <AddNewPawfileButton/>
+            <button onClick={()=> this.props.dispatch(showPawfileForm(true, undefined))} className="tape add-new-pawfile">Add New Pawfile</button>
           </nav>
           {/* only allow user to sort if there's more than one pet */}
           <PawfilesList/>
